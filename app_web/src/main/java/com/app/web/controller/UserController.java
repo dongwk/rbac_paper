fe7dd04c4b42ebb6d197f41bfd08639f7ba246c5
dwk <dwk126@126.com>
2018-01-17 01:13:25 +0800

    spring boot

--- app_web/src/main/java/com/app/web/controller/UserController.java
+++ app_web/src/main/java/com/app/web/controller/UserController.java
@@ -6,19 +6,23 @@ package com.app.web.controller;
 import com.app.service.service.UserService;
 import com.app.web.base.BaseController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@EnableAutoConfiguration
 public class UserController extends BaseController{
-	
-	@Autowired
-	private UserService userService;
 	
 	@RequestMapping("/{id}")
 	public String index(@PathVariable long id){
     	return id+"";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserController.class);
 	}
 }
