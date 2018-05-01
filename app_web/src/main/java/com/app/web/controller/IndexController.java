5045d5d25c7c99a6c067afb051a0fe3a311ffaa1
dwk <dwk126@126.com>
2018-05-02 01:13:17 +0800

    druid 各种配置 filter StatFilter 监控配置 WallFilter SQL注入攻击

---
+++ app_web/src/main/java/com/app/web/controller/IndexController.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.app.web.controller;
+
+import com.app.model.model.User;
+import com.app.service.service.UserService;
+import com.app.web.base.BaseController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Controller
+public class IndexController extends BaseController{
+
+	@GetMapping(value = "/")
+	public String get(){
+		System.out.println("-----111222");
+		return "index";
+	}
+}
